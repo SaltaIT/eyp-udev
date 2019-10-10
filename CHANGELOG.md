aa15bf8b08211dae2c0bbc3110a0666c979b34aa
Jordi Prats <jordi.prats@gmail.com>
2019-10-10 13:24:49 +0200

    cleanup + rhel 8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,17 @@
 # CHANGELOG
 
+## 0.1.6
+
+*  added support for **RHEL 8** and **Debian 10**
+
 ## 0.1.5
 
-*  added SLES 13.4
+*  added **SLES 13.4**
 
 ## 0.1.4
 
-*  added SLES 11.3 and 12.3 support
+*  added **SLES 11.3** and **12.3** support
 
 ## 0.1.3
 
-*  added Ubuntu 18.04 support
+*  added **Ubuntu 18.04** support
--- manifests/params.pp
+++ manifests/params.pp
@@ -6,57 +6,12 @@ class udev::params {
   {
     'redhat' :
     {
-      case $::operatingsystemrelease
-      {
-        /^[567].*$/:
-        {
-        }
-        default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
-      }
     }
     'Debian':
     {
-      case $::operatingsystem
-      {
-        'Ubuntu':
-        {
-          case $::operatingsystemrelease
-          {
-            /^1[468].*$/:
-            {
-            }
-            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
-          }
-        }
-        'Debian':
-        {
-          case $::operatingsystemrelease
-          {
-            /^[89].*$/:
-            {
-            }
-            default: { fail("Unsupported Debian version! - ${::operatingsystemrelease}")  }
-          }
-        }
-        default: { fail('Unsupported Debian flavour!')  }
-      }
     }
     'Suse':
     {
-      case $::operatingsystem
-      {
-        'SLES':
-        {
-          case $::operatingsystemrelease
-          {
-            /^1[12].[34]$/:
-            {
-            }
-            default: { fail("Unsupported operating system ${::operatingsystem} ${::operatingsystemrelease}") }
-          }
-        }
-        default: { fail("Unsupported operating system ${::operatingsystem}") }
-      }
     }
     default  : { fail('Unsupported OS!') }
   }
